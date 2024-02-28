d9663d47e9e8cc897cd718573516f32f5fdfbba0
Harsha_Medicharla <harshamedicharla05@gmail.com>
2024-02-28 13:54:37 +0530

    modified issue 3

--- issue3.c
+++ issue3.c
@@ -1,4 +1,4 @@
-// Make sure the program prints the string correctly
+/// Make sure the program prints the string correctly
 
 #include <stdio.h>
 
@@ -6,10 +6,10 @@ int main(){
     // Printing all the characters in an array
     char z[] = {'h', 'e', 'l', 'l', 'o', '!'};
 
-    char new[50] = ""; 
+    char new[50] = "";
 
     for(int i = 0; i < 6; i++){
-        new[i] =  z;
+        new[i] =  z[i];//we should mention the index of z
     }
 
     for(int i = 0; i < 6; i++){
