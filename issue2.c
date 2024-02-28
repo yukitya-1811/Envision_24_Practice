a94aa37b4049e4798f4aa9c3481482eeae14e257
Harsha_Medicharla <harshamedicharla05@gmail.com>
2024-02-28 13:52:29 +0530

    modified issue 2

--- issue2.c
+++ issue2.c
@@ -1,12 +1,12 @@
-// Make sure program prints all even numbers from 1 to 40
+/// Make sure program prints all even numbers from 1 to 40
 
 #include <stdio.h>
 
 int main(){
     int even = 0;
     for(int i = 0; i <= 20; i++){
-        even *= 2;
-        printf("%d\t", even);
+       printf("%d\t", even);
+               even+=2;//updation error
     }
     printf("\n");
     return 0;
